b31be7b4db403c92b95c0064260c97620c132d08
Andy Fragen <andy@thefragens.com>
2015-11-11 19:32:20 -0800

    make templates match style

--- templates/lrhoa-subdir.php
+++ templates/lrhoa-subdir.php
@@ -18,16 +18,24 @@ get_header(); ?>
 
 		<div id="primary">
 			<div id="content" role="main">
+				<article>
 
-			<?php while ( have_posts() ) : the_post(); ?>
-				<?php get_template_part( 'content', 'page' ); ?>
-			<?php endwhile; // end of the loop. ?>
+					<?php while ( have_posts() ) : the_post(); ?>
+					<header class="entry-header">
+						<h1 class="entry-title"><?php echo $post->post_title; ?></h1>
+					</header>
+					<div class="entry-content">
 
+						<?php get_template_part( 'content', 'page' ); ?>
+						<?php endwhile; // end of the loop. ?>
 
-<?php $default_dir = "/lrhoa-docs/" . $name . "/"; ?>
-<?php include_once( locate_template( './templates/list-files.php' ) ); ?>
+						<?php $default_dir = "/lrhoa-docs/" . $name . "/"; ?>
+
+						<?php include_once( locate_template( './templates/list-files.php' ) ); ?>
+				</article>
 
 			</div><!-- #content -->
 		</div><!-- #primary -->
 
+<?php get_sidebar(); ?>
 <?php get_footer(); ?>
