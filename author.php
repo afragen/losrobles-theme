5fadd0c5906f5037533783459668afece0ea42df
Andy Fragen <andy@thefragens.com>
2020-02-23 12:59:45 -0800

    update author.php from dependence on Pods

--- author.php
+++ author.php
@@ -6,7 +6,6 @@
 get_header();
 global $author_name;
 
-$pods = pods( 'user', get_query_var( 'post_type_author' ), true );
 $member = get_user_by( 'slug', $author_name );
 
 ?>
@@ -19,17 +18,16 @@ $member = get_user_by( 'slug', $author_name );
 						<h1 class="entry-title"><?php echo 'Member Contact Information'; ?></h1>
 					</header>
 					<div class="lrhoa_member_data">
-						<?php  if ( $pods && is_user_logged_in() && current_user_can( 'members' ) ) {
-						$my_pods_user = pods( 'user', $member->ID );
+						<?php
+						if ( is_user_logged_in() && current_user_can( 'members' ) ) {
+							print esc_attr( $member->data->display_name ) . '<br>';
+							print esc_attr( $member->get( 'lrhoa_street_number' ) ) . ' Los Robles<br>';
+							print esc_attr( $member->get( 'lrhoa_phone_number' ) ) . '<br>';
+							print '<a href="mailto:">' . esc_attr( $member->data->user_email ) . '</a><br>';
 
-						print( esc_attr( $member->data->display_name ) . '<br>' );
-						print( esc_attr( $my_pods_user->field('street_number') ) . ' Los Robles<br>' );
-						print( esc_attr( $my_pods_user->field('phone_number') ). '<br>' );
-						print( '<a href="mailto:">' . esc_attr( $member->data->user_email ) . '</a><br>' );
-
-						print( 'Emergency Contact: ' . esc_attr( $my_pods_user->field('emergency_contact_phone') ) . '<br>' );
+							print 'Emergency Contact: ' . esc_attr( $member->get( 'lrhoa_emergency_number' ) ) . '<br>';
 						} else {
-							print( 'You must log in to view this page.' );
+							print 'You must log in to view this page.';
 						}
 						?>
 					</div>
